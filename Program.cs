da38705d4f8792445a5b2a3886de25c2542fa1ad
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-28 08:39:39 +0400

    Task3.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _28._03_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sentence = "I am studying at the Azerbaijan Technical University";
+            Console.WriteLine(Sentence);
+            Console.WriteLine("First Word: ");
+            Console.WriteLine(FindFirstWord(Sentence));
+        }
+        public static string FindFirstWord(string sentence)
+        {
+            string[] word = sentence.Split();
+            return word[0];
+        }
+    }
+}
